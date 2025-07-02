2db53373994d6ec4285fde6a112e22a860d032fc
abdullah8emin <eminsisman2003@gmail.com>
2025-07-02 12:33:27 +0300

    tum verileri cekiyor tek barkod duzenlemesi yapilcak

--- dermoeczanem_veri/dermoeczanem_csv.py
+++ dermoeczanem_veri/dermoeczanem_csv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # JSON dosyasını oku
-df = pd.read_json("dermoecz_all_products.json", encoding="utf-8")
+df = pd.read_json("dermoeczanem_all_products.json", encoding="utf-8")
 
 # CSV olarak kaydet
-df.to_csv("dermoecz_urun_katalog.csv", index=False, encoding="utf-8-sig")
+df.to_csv("dermoeczanem_urun_katalog.csv", index=False, encoding="utf-8-sig")
 
 print("✅ CSV dosyası başarıyla oluşturuldu: recetem_com_urun_katalog.csv")
